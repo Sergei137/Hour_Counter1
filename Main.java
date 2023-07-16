38a7ddacd7e56874e5ffba11e46ce8345a7d0272
Sam <sm1.618@protonmail.com>
2023-07-16 16:31:40 -0600

    progress commit

--- Main.java
+++ Main.java
@@ -1,20 +1,84 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("Hours Counter Calculator");
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        MainMenu(scanner1);
+
+        scanner1.close();        
+    }
+
+    // Main Menu Method
+    public static void MainMenu(Scanner scanner1){
+        
+        int input1 = 0;
+
+        System.out.println("Main Menu");
+        System.out.println("1. New Entry");
+        input1 = scanner1.nextInt();
+        
+       
+        
+        if (input1 == 1){
+            NewEntry(scanner1);
+        }
+
+        
+    }
+
+    // New Entry Method
+    public static void NewEntry(Scanner scanner1){
+
+        int year1 = 0;
+        String month1 = "";
+        int monthDay1 = 0;
+        String day1 = "";
+        int totalHours = 0;
+        int hourStart = 0;
+        int hourEnd = 0;
+
+        System.out.print("Enter current year: ");
+        year1 = scanner1.nextInt();
+        System.out.println("Enter current month: ");
+        month1 = scanner1.nextLine();
+        System.out.println("Enter current day of the month: ");
+        monthDay1 = scanner1.nextInt();
+        System.out.println("Enter current day of the week: ");
+        day1 = scanner1.nextLine();
+        System.out.println("Enter starting hour: ");
+        hourStart = scanner1.nextInt();
+        System.out.println("Enter the ending hour: ");
+
+
+
+
+        
+
+        System.out.println("Hours entered: ");
+    }
+
+    // View entry by date
+    public static void ViewEntry(Scanner scanner1){
+
+    
+
+    }
+
+    // Write to file
+    public static void WriteToFile(){
+        
+    }
+
+}
+
+
+
 
 // Hour Counter Program
 // Author: Sergei Mochalov
 // Created: 2023-07-15
 // Updated: 2023-07-15
-
-public class Main{
-
-    static int totalHours = 0;
-    static int hours1 = 0;
-
-
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-
-
-
-
-}
