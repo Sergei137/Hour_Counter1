ddf8798462847a6665d738c773cc77b0224ce185
Sam <sm1.618@protonmail.com>
2023-07-15 18:29:54 -0600

    changed comments

--- main.java
+++ main.java
@@ -1,10 +1,15 @@
 
-// hello world program
+// Hour Counter Program
 // Author: Sergei Mochalov
-// Created: 2023-08-20
-// Updated: 2023-08-20
+// Created: 2023-07-15
+// Updated: 2023-07-15
 
 public class main{
+
+    int totalHours = 0;
+    int hours1 = 0;
+
+
     public static void main(String[] args){
         System.out.println("Hello World");
     }
